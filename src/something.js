73c9bb04b935658154208cf2fe1cc6ceb40fbfc5
SualehaSehar <66309175+SualehaSehar@users.noreply.github.com>
2020-06-10 21:28:53 +0500

    after session 3 assignment
    made a new file something.js and did changes in app.js

---
+++ src/something.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Quote(props) {
+     return(
+        <div>
+           <h1>"Dont give up. {props.textName} "</h1> 
+           <h1>"Keep going."</h1>
+        </div> 
+     );
+}
+export default Quote;
